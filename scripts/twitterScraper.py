6aecbe65663b9d7bffae409d4191cfcdb2f594ac
Sejal K. Parmar <skparmar701@gmail.com>
2020-04-22 20:36:08 -0500

    updating configs

--- scripts/twitterScraper.py
+++ scripts/twitterScraper.py
@@ -5,11 +5,12 @@ import string
 import pandas as pd
 import os
 import json
+from datetime import datetime
 
 # from pymongo import MongoClient
 # from dotenv import load_dotenv
 
-from sentiment import text_to_sentiment
+# from sentiment import text_to_sentiment
 
 with open("env_vars.json", "r") as config:
     contents = config.read()
@@ -77,7 +78,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # process sanders tweets
-tweet_csv = open('tweets_full.csv', 'a', encoding='UTF-8', newline='')
+tweet_csv = open(f'tweets_full_{date}.csv', 'a', encoding='UTF-8', newline='')
 tweet_csvWriter = csv.writer(tweet_csv)
 tweet_csvWriter.writerow(
     ['tweet_id', 'date', 'candidate', 'party', 'user_name', 'user_location', 'text', 'sentiment', 'tokens', 'likes',
@@ -86,88 +87,88 @@ count = 0
 
 last_seen_bernie = last_sanders_id
 # use a radius that covers all of illinois, pull 100 most recent tweets
-for tweet in tweepy.Cursor(api.search, q="bernie sanders", geocode='40.049174,-88.858617,193mi', since_id=last_sanders_id, count=1000000).items(
-        1000000):
-    last_seen_bernie = tweet.id
-    tweet_txt = tweet.text
-    name = tweet.user.name
-    user_loc = ''
-    lat = 0
-    long = 0
-    if 'bernie' not in tweet_txt.lower() and 'sanders' not in tweet_txt.lower():
-        continue
-    if tweet.place != None:
-        full_location = tweet.place.full_name
-        l = full_location.split()
-        if "IL" in l[1]:
-            res = cities_df[cities_df['city'] == tweet.place.name]
-            lat = res.iloc[0]['lat']
-            long = res.iloc[0]['lng']
-            user_loc = tweet.place.name
-            sentiment_tokens = text_to_sentiment(tweet_txt)
-            if sentiment_tokens == "no valid words in text":
-                continue
-            tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Democrat", "Sanders",
-                              tweet_txt, sentiment_tokens[0], sentiment_tokens[1], user_loc)
-            print("writing row, bernie ")
-            tweet_csvWriter.writerow(tweet_obj.format_csv_row())
-            # collection.insert_one(tweet_obj.format_json())
+# for tweet in tweepy.Cursor(api.search, q="bernie sanders", geocode='40.049174,-88.858617,193mi', since_id=last_sanders_id, count=1000000).items(
+#         1000000):
+#     last_seen_bernie = tweet.id
+#     tweet_txt = tweet.text
+#     name = tweet.user.name
+#     user_loc = ''
+#     lat = 0
+#     long = 0
+#     if 'bernie' not in tweet_txt.lower() and 'sanders' not in tweet_txt.lower():
+#         continue
+#     if tweet.place != None:
+#         full_location = tweet.place.full_name
+#         l = full_location.split()
+#         if "IL" in l[1]:
+#             res = cities_df[cities_df['city'] == tweet.place.name]
+#             lat = res.iloc[0]['lat']
+#             long = res.iloc[0]['lng']
+#             user_loc = tweet.place.name
+#             sentiment_tokens = text_to_sentiment(tweet_txt)
+#             if sentiment_tokens == "no valid words in text":
+#                 continue
+#             tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Democrat", "Sanders",
+#                               tweet_txt, sentiment_tokens[0], sentiment_tokens[1], user_loc)
+#             print("writing row, bernie ")
+#             tweet_csvWriter.writerow(tweet_obj.format_csv_row())
 
 last_seen_biden = last_biden_id
-for tweet in tweepy.Cursor(api.search, q="joe biden", geocode='40.049174,-88.858617,193mi', since_id=last_biden_id, count=1000000).items(
-        1000000):
-    last_seen_biden = tweet.id
-    tweet_txt = tweet.text
-    name = tweet.user.name
-    user_loc = ''
-    lat = 0
-    long = 0
-    if 'joe' not in tweet_txt.lower() and 'biden' not in tweet_txt.lower():
-        continue
-    if tweet.place != None:
-        full_location = tweet.place.full_name
-        l = full_location.split()
-        if "IL" in l[1]:
-            res = cities_df[cities_df['city'] == tweet.place.name]
-            lat = res.iloc[0]['lat']
-            long = res.iloc[0]['lng']
-            user_loc = tweet.place.name
-            sentiment_tokens = text_to_sentiment(tweet_txt)
-            if sentiment_tokens == "no valid words in text":
-                continue
-            tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Democrat", "Biden",
-                          tweet_txt, sentiment_tokens[0], sentiment_tokens[1], user_loc)
-            print("writing row, joe")
-            tweet_csvWriter.writerow(tweet_obj.format_csv_row())
-            # collection.insert_one(tweet_obj.format_json())
+# for tweet in tweepy.Cursor(api.search, q="joe biden", geocode='40.049174,-88.858617,193mi', since_id=last_biden_id, count=1000000).items(
+#         1000000):
+#     last_seen_biden = tweet.id
+#     tweet_txt = tweet.text
+#     name = tweet.user.name
+#     user_loc = ''
+#     lat = 0
+#     long = 0
+#     if 'joe' not in tweet_txt.lower() and 'biden' not in tweet_txt.lower():
+#         continue
+#     if tweet.place != None:
+#         full_location = tweet.place.full_name
+#         l = full_location.split()
+#         if "IL" in l[1]:
+#             res = cities_df[cities_df['city'] == tweet.place.name]
+#             lat = res.iloc[0]['lat']
+#             long = res.iloc[0]['lng']
+#             user_loc = tweet.place.name
+#             sentiment_tokens = text_to_sentiment(tweet_txt)
+#             if sentiment_tokens == "no valid words in text":
+#                 continue
+#             tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Democrat", "Biden",
+#                           tweet_txt, sentiment_tokens[0], sentiment_tokens[1], user_loc)
+#             print("writing row, joe")
+#             tweet_csvWriter.writerow(tweet_obj.format_csv_row())
 
 last_seen_trump = last_trump_id
-for tweet in tweepy.Cursor(api.search, q="donald trump", geocode='40.049174,-88.858617,193mi', ince_id=last_trump_id, count=1000000).items(
-        1000000):
-    last_seen_trump = tweet.id
-    tweet_txt = tweet.text
-    name = tweet.user.name
-    user_loc = ''
-    lat = 0
-    long = 0
-    if 'donald' not in tweet_txt.lower() and 'trump' not in tweet_txt.lower():
-        continue
-    if tweet.place != None:
-        full_location = tweet.place.full_name
-        l = full_location.split()
-        if "IL" in l[1]:
-            res = cities_df[cities_df['city'] == tweet.place.name]
-            lat = res.iloc[0]['lat']
-            long = res.iloc[0]['lng']
-            user_loc = tweet.place.name
-            sentiment_tokens = text_to_sentiment(tweet_txt)
-            if sentiment_tokens == "no valid words in text":
-                continue
-            tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Republican", "Trump",
-                          tweet_txt, sentiment_tokens[0], sentiment_tokens[1],user_loc)
-            print("writing row, trump")
-            tweet_csvWriter.writerow(tweet_obj.format_csv_row())
+# for tweet in tweepy.Cursor(api.search, q="donald trump", geocode='40.049174,-88.858617,193mi', ince_id=last_trump_id, count=1000000).items(
+#         1000000):
+#     last_seen_trump = tweet.id
+#     tweet_txt = tweet.text
+#     name = tweet.user.name
+#     user_loc = ''
+#     lat = 0
+#     long = 0
+#     if 'donald' not in tweet_txt.lower() and 'trump' not in tweet_txt.lower():
+#         continue
+#     if tweet.place != None:
+#         full_location = tweet.place.full_name
+#         l = full_location.split()
+#         if "IL" in l[1]:
+#             res = cities_df[cities_df['city'] == tweet.place.name]
+#             lat = res.iloc[0]['lat']
+#             long = res.iloc[0]['lng']
+#             user_loc = tweet.place.name
+#             sentiment_tokens = text_to_sentiment(tweet_txt)
+#             if sentiment_tokens == "no valid words in text":
+#                 continue
+#             tweet_obj = Tweet(tweet.id, tweet.created_at, lat, long, name, tweet.favorite_count, "Republican", "Trump",
+#                           tweet_txt, sentiment_tokens[0], sentiment_tokens[1],user_loc)
+#             print("writing row, trump")
+#             tweet_csvWriter.writerow(tweet_obj.format_csv_row())
 
-new_vars = {"last_sanders_id": last_seen_bernie,"last_biden_id": last_seen_biden,"last_trump_id": last_seen_trump}
+now = datetime.now()
+new_date = now.strftime("%m_%d_%Y")
+new_vars = {"last_sanders_id": last_seen_bernie,"last_biden_id": last_seen_biden,"last_trump_id": last_seen_trump, "date": new_date}
 with open("env_vars.json", "w") as config:
     json.dump(new_vars, config)
